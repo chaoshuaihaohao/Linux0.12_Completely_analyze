367a648176ea8fd446a0ae563a5f9454139e2ede
chaoshuaihaohao <chenhao199408@163.com>
2019-11-12 23:09:45 +0800

    Modified the code style and the cyclomatic complexity of build.c
    Signed-off-by: chaoshuaihaohao <chenhao199408@163.com>

--- tools/build.c
+++ tools/build.c
@@ -1,3 +1,4 @@
+
 /*
  *  linux/tools/build.c
  *
@@ -31,13 +32,13 @@
 #include <unistd.h>	/* contains read/write */
 #include <fcntl.h>
 
-#define MAJOR(a) (((unsigned)(a))>>8)
-#define MINOR(a) ((a)&0xff)
+#define MAJOR(a) (((unsigned)(a)) >> 8)
+#define MINOR(a) ((a) & 0xff)
 
 #define MINIX_HEADER 32
-#define GCC_HEADER (1024+0xc00)
+#define GCC_HEADER (1024 + 0xc00)
 
-#define SYS_SIZE 0x3000
+#define SYS_SIZE 0x3000	/* 12 * 1024 */
 
 #define DEFAULT_MAJOR_ROOT 3
 #define DEFAULT_MINOR_ROOT 6
@@ -51,9 +52,9 @@
 
 #define STRINGIFY(x) #x
 
-void die(char * str)
+void die(char *str)
 {
-	fprintf(stderr,"%s\n",str);
+	fprintf(stderr, "%s\n", str);
 	exit(1);
 }
 
@@ -62,16 +63,15 @@ void usage(void)
 	die("Usage: build bootsect setup system [rootdev] [> image]");
 }
 
-int main(int argc, char ** argv)
+static void build_bootsect(int argc, char **argv)
 {
-	int i,c,id;
-	char buf[4096];
 	char major_root, minor_root;
 	char major_swap, minor_swap;
 	struct stat sb;
+	char buf[1024];
+	int id;
+	int i;
 
-	if ((argc < 4) || (argc > 6))
-		usage();
 	if (argc > 4) {
 		if (strcmp(argv[4], "FLOPPY")) {
 			if (stat(argv[4], &sb)) {
@@ -106,96 +106,125 @@ int main(int argc, char ** argv)
 	}
 	fprintf(stderr, "Root device is (%d, %d)\n", major_root, minor_root);
 	fprintf(stderr, "Swap device is (%d, %d)\n", major_swap, minor_swap);
-	if ((major_root != 2) && (major_root != 3) &&
-	    (major_root != 0)) {
-		fprintf(stderr, "Illegal root device (major = %d)\n",
-			major_root);
+	if ((major_root != 2) && (major_root != 3) && (major_root != 0)) {
+		fprintf(stderr, "Illegal root device (major = %d)\n", major_root);
 		die("Bad root device --- major #");
 	}
 	if (major_swap && major_swap != 3) {
-		fprintf(stderr, "Illegal swap device (major = %d)\n",
-			major_swap);
+		fprintf(stderr, "Illegal swap device (major = %d)\n", major_swap);
 		die("Bad root device --- major #");
 	}
-	for (i=0;i<sizeof buf; i++) buf[i]=0;
-	if ((id=open(argv[1],O_RDONLY,0))<0)
+	for (i = 0; i < sizeof(buf); i++)
+		buf[i] = 0;
+	id = open(argv[1], O_RDONLY, 0);
+	if (id < 0)
 		die("Unable to open 'boot'");
-	if (read(id,buf,MINIX_HEADER) != MINIX_HEADER)
+	i = read(id , buf, MINIX_HEADER);
+	if (i != MINIX_HEADER)
 		die("Unable to read header of 'boot'");
-	if (((long *) buf)[0]!=0x04100301)
+	if (((long *)buf)[0] != 0x04100301)
 		die("Non-Minix header of 'boot'");
-	if (((long *) buf)[1]!=MINIX_HEADER)
+	if (((long *)buf)[1] != MINIX_HEADER)
 		die("Non-Minix header of 'boot'");
-	if (((long *) buf)[3]!=0)
+	if (((long *)buf)[3] != 0)
 		die("Illegal data segment in 'boot'");
-	if (((long *) buf)[4]!=0)
+	if (((long *)buf)[4] != 0)
 		die("Illegal bss in 'boot'");
-	if (((long *) buf)[5] != 0)
+	if (((long *)buf)[5] != 0)
 		die("Non-Minix header of 'boot'");
-	if (((long *) buf)[7] != 0)
+	if (((long *)buf)[7] != 0)
 		die("Illegal symbol table in 'boot'");
-	i=read(id,buf,sizeof buf);
+	i = read(id, buf, sizeof(buf));
 	fprintf(stderr,"Boot sector %d bytes.\n",i);
 	if (i != 512)
 		die("Boot block must be exactly 512 bytes");
-	if ((*(unsigned short *)(buf+510)) != 0xAA55)
+	if ((*(unsigned short *)(buf + 510)) != 0xAA55)
 		die("Boot block hasn't got boot flag (0xAA55)");
-	buf[506] = (char) minor_swap;
-	buf[507] = (char) major_swap;
-	buf[508] = (char) minor_root;
-	buf[509] = (char) major_root;	
-	i=write(1,buf,512);
-	if (i!=512)
+	buf[506] = (char)minor_swap;
+	buf[507] = (char)major_swap;
+	buf[508] = (char)minor_root;
+	buf[509] = (char)major_root;
+	i = write(1, buf, 512);
+	if (i != 512)
 		die("Write call failed");
 	close (id);
-	
-	if ((id=open(argv[2],O_RDONLY,0))<0)
+}
+
+static void build_setup(char **argv)
+{
+	char buf[1024];
+	int c, i;
+	int id;
+
+	id = open(argv[2], O_RDONLY, 0);
+	if (id < 0)
 		die("Unable to open 'setup'");
-	if (read(id,buf,MINIX_HEADER) != MINIX_HEADER)
+	c = read(id, buf, MINIX_HEADER);
+	if (c != MINIX_HEADER)
 		die("Unable to read header of 'setup'");
-	if (((long *) buf)[0]!=0x04100301)
+	if (((long *)buf)[0] != 0x04100301)
 		die("Non-Minix header of 'setup'");
-	if (((long *) buf)[1]!=MINIX_HEADER)
+	if (((long *)buf)[1] != MINIX_HEADER)
 		die("Non-Minix header of 'setup'");
-	if (((long *) buf)[3]!=0)
+	if (((long *)buf)[3] != 0)
 		die("Illegal data segment in 'setup'");
-	if (((long *) buf)[4]!=0)
+	if (((long *)buf)[4] != 0)
 		die("Illegal bss in 'setup'");
-	if (((long *) buf)[5] != 0)
+	if (((long *)buf)[5] != 0)
 		die("Non-Minix header of 'setup'");
-	if (((long *) buf)[7] != 0)
+	if (((long *)buf)[7] != 0)
 		die("Illegal symbol table in 'setup'");
-	for (i=0 ; (c=read(id,buf,sizeof buf))>0 ; i+=c )
-		if (write(1,buf,c)!=c)
+	for (i = 0; (c = read(id, buf, sizeof(buf))) > 0; i += c)
+		if (write(1, buf, c) != c)
 			die("Write call failed");
 	close (id);
-	if (i > SETUP_SECTS*512)
+	if (i > SETUP_SECTS * 512)
 		die("Setup exceeds " STRINGIFY(SETUP_SECTS)
-			" sectors - rewrite build/boot/setup");
-	fprintf(stderr,"Setup is %d bytes.\n",i);
-	for (c=0 ; c<sizeof(buf) ; c++)
+		    " sectors - rewrite build/boot/setup");
+	fprintf(stderr, "Setup is %d bytes.\n", i);
+	for (c = 0; c < sizeof(buf); c++)
 		buf[c] = '\0';
-	while (i<SETUP_SECTS*512) {
-		c = SETUP_SECTS*512-i;
+	while (i < SETUP_SECTS * 512) {
+		c = SETUP_SECTS * 512 - i;
 		if (c > sizeof(buf))
 			c = sizeof(buf);
-		if (write(1,buf,c) != c)
+		if (write(1, buf, c) != c)
 			die("Write call failed");
 		i += c;
 	}
-	
-	if ((id=open(argv[3],O_RDONLY,0))<0)
+}
+static void build_system(char **argv)
+{
+	char buf[4096];
+	int c, i;
+	int id;
+
+	id = open(argv[3],O_RDONLY,0);
+	if (id < 0)
 		die("Unable to open 'system'");
-	if (read(id,buf,GCC_HEADER) != GCC_HEADER)
+	if (read(id, buf, GCC_HEADER) != GCC_HEADER)
 		die("Unable to read header of 'system'");
 //	if (((long *) buf)[5] != 0)
 //		die("Non-GCC header of 'system'");
-	for (i=0 ; (c=read(id,buf,sizeof buf))>0 ; i+=c )
-		if (write(1,buf,c)!=c)
+	for (i = 0; (c = read(id, buf, sizeof(buf))) > 0 ; i += c)
+		if (write(1, buf, c) != c)
 			die("Write call failed");
 	close(id);
 	fprintf(stderr,"System is %d bytes.\n",i);
-	if (i > SYS_SIZE*16)
+	if (i > SYS_SIZE * 16)
 		die("System is too big");
-	return(0);
+}
+
+int main(int argc, char ** argv)
+{
+	if ((argc < 4) || (argc > 6))
+		usage();
+
+	build_bootsect(argc, argv);
+
+	build_setup(argv);
+
+	build_system(argv);
+
+	return 0;
 }
